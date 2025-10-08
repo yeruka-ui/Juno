976bdb38bc368dae67df19ea6d1d6c1a4b920297
PC <silveriolance06@gmail.com>
2025-10-08 22:51:34 +0800

    Optimized json handling and chat bubble ui, updated requirement.txt

--- static/script/botMsg.js
+++ static/script/botMsg.js
@@ -1,7 +1,7 @@
 //bot message div
 export function botMsg(msg) {
     const message = document.createElement('div')
-    message.className = "h-fit w-[85%] sm:w-[700px] bg-gradient-to-r from-emerald-100 to-teal-100 rounded-2xl self-start drop-shadow text-[20px] text-start sm:text-2xl p-2 sm:p-6 text-wrap text-slate-600"
+    message.className = "h-fit w-fit bg-gradient-to-r from-emerald-100 to-teal-100 rounded-2xl self-start drop-shadow text-[20px] text-start sm:text-2xl p-2 sm:p-6 text-wrap text-slate-600"
     message.innerHTML = msg;
 
     return message;
@@ -9,7 +9,7 @@ export function botMsg(msg) {
 //bot message error
 export function errorMsg(error) {
     const message = document.createElement('div')
-    message.className = "h-fit w-[85%] sm:w-[700px] bg-red-100 rounded-2xl self-start drop-shadow text-[20px] text-start sm:text-2xl p-2 sm:p-6 text-wrap text-red-600"
+    message.className = "h-fit w-fit bg-red-100 rounded-2xl self-start drop-shadow text-[20px] text-start sm:text-2xl p-2 sm:p-6 text-wrap text-red-600"
     message.innerHTML = error;
 
     return message;
--- static/script/humanMsg.js
+++ static/script/humanMsg.js
@@ -1,6 +1,6 @@
 export default function humanMsg(msg) {
     const message = document.createElement('div')
-    message.className = "h-fit w-[85%] sm:w-[700px] bg-white rounded-2xl self-end drop-shadow font-[Inter] text-[20px] text-start sm:text-2xl p-2 sm:p-6 text-wrap text-slate-600"
+    message.className = "h-fit w-fit bg-white rounded-2xl self-end drop-shadow font-[Inter] text-[20px] text-start sm:text-2xl p-2 sm:p-6 text-wrap text-slate-600"
     message.innerHTML = msg;
 
     return message;
