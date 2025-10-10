a8c85e58137892cf6e359917530668e68916f468
PC <silveriolance06@gmail.com>
2025-10-10 23:53:08 +0800

    minor ui chatbubble adjustment

--- static/script/botMsg.js
+++ static/script/botMsg.js
@@ -2,7 +2,7 @@ import { chatAnimation } from "./animationHandler.js";
 
 export function botMsg(msg) {
     const message = document.createElement('div');
-    message.className = "chat h-fit w-fit bg-gradient-to-r from-emerald-100 to-teal-100 rounded-2xl self-start drop-shadow text-[20px] text-start sm:text-2xl p-2 sm:p-6 text-wrap text-slate-600";
+    message.className = "chat h-fit w-fit max-w-[95%] bg-gradient-to-r from-emerald-100 to-teal-100 rounded-2xl self-start drop-shadow text-[20px] text-start sm:text-2xl p-2 sm:p-6 text-wrap text-slate-600";
     message.innerHTML = msg;
     chatAnimation(message);
     return message;
@@ -10,7 +10,7 @@ export function botMsg(msg) {
 
 export function errorMsg(error) {
     const message = document.createElement('div');
-    message.className = "chat h-fit w-fit bg-red-100 rounded-2xl self-start drop-shadow text-[20px] text-start sm:text-2xl p-2 sm:p-6 text-wrap text-red-600";
+    message.className = "chat h-fit w-fit max-w-[95%] bg-red-100 rounded-2xl self-start drop-shadow text-[20px] text-start sm:text-2xl p-2 sm:p-6 text-wrap text-red-600";
     message.innerHTML = error;
     chatAnimation(message);
     return message;
