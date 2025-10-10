ced0ac8699488b0b835bcf28666d506eda2b7ae0
PC <silveriolance06@gmail.com>
2025-10-10 23:39:58 +0800

    added basic animation to main and login

--- static/script/humanMsg.js
+++ static/script/humanMsg.js
@@ -1,7 +1,9 @@
+import {chatAnimation} from "./animationHandler.js";
+
 export default function humanMsg(msg) {
     const message = document.createElement('div')
     message.className = "h-fit w-fit bg-white rounded-2xl self-end drop-shadow font-[Inter] text-[20px] text-start sm:text-2xl p-2 sm:p-6 text-wrap text-slate-600"
     message.innerHTML = msg;
-
+    chatAnimation(message);
     return message;
 }
